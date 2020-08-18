f402922b53bc37347186321501bb561b8399d65d
Joaquín Rivera <jeriveramoya@suse.com>
2020-08-18 11:10:39 +0200

    Unify launching of the YaST modules from ruby

--- spec/expert_partitioner/expert_partitioner_spec.rb
+++ spec/expert_partitioner/expert_partitioner_spec.rb
@@ -5,13 +5,19 @@ require 'libyui_test_framework'
 module LibyuiTestFramework
   RSpec.describe "Once Expert partitioner is launched" do
     before :all do
-      @app = LibyuiClient::App.new(host: '192.168.200.179' , port: '9999')
+      @app = LibyuiClient::App.new(host: '192.168.200.146' , port: '9999')
+      
+      local_process = LibyuiClient::LocalProcess.new
+      local_process.start_app('YUI_REUSE_PORT=1 YUI_HTTP_PORT=9999 yast2 partitioner --qt')
+      local_process.start_app("xterm -e 'YUI_REUSE_PORT=1 YUI_HTTP_PORT=9999 yast2 partitioner --ncurses'")
+      
       @distri = Distributions::DistributionProvider.provide
       @expert_partitioner = @distri.get_expert_partitioner(@app)
     end
 
     after :all do
       # ensure module is closed
+      @local_process.kill_app
     end
 
     it "creates default partitioning on 2nd disk" do
